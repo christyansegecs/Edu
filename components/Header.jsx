20c2f4a2d63579ccdc2b322bd6a285d72f950bfa
christyansegecs <christyansegecs@hotmail.com>
2024-10-02 00:17:07 -0300

    Initial Commit

---
+++ components/Header.jsx
@@ -0,0 +1,33 @@
+
+import Link from 'next/link'
+import { Nav } from '../components/Nav'
+import { MobileNav } from '../components/MobileNav'
+import { Button } from './ui/button'
+
+
+export function Header() {
+
+    return (
+        <header className='py-8 xl:py-12 text-white bg-pink-50/20'>
+            <div className='container mx-auto flex justify-between items-center'>
+                <Link href='/'>
+                    <h1 className='text-4xl font-semibold'>
+                        Edu<span className='text-accent'>.</span>
+                    </h1>
+                </Link>
+
+                <div className='hidden xl:flex items-center gap-8'>
+                    <Nav />
+                    <Link href='/contact'>
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                <div className='xl:hidden'>
+                    <MobileNav />
+                </div>
+
+            </div>
+        </header>
+    )
+}
---
+++ components/Nav.jsx
@@ -0,0 +1,51 @@
+
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const links = [
+    {
+        name: 'home',
+        path: '/'
+    },
+    {
+        name: 'propostas',
+        path: '/services'
+    },
+    {
+        name: 'partido',
+        path: '/resume'
+    },
+    {
+        name: 'projetos',
+        path: '/work'
+    },
+    {
+        name: 'contact',
+        path: '/contact'
+    },
+]
+
+export function Nav() {
+
+    const pathname = usePathname()
+
+    return (
+        <nav className='flex gap-8'>
+            {links.map((link, index) => {
+                return (
+                    <Link 
+                        href={link.path} 
+                        key={index} 
+                        className={`${
+                            link.path === pathname && 'text-accent border-b-2 border-accent'
+                            } capitalize font-medium hover:text-accent transition-all`}
+                    >
+                        {link.name}
+                    </Link>
+                )
+            })}
+        </nav>
+    )
+}
